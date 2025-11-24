2ea84a3cb3f5b415e547615f430cc3e0338d31d3
Rodolpho Dutra <31833774+rodolphodutra@users.noreply.github.com>
2025-11-24 15:10:30 -0300

    Add error handling and download Metabase script

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+set -e
+
+echo "Baixando Metabase..."
 curl -L https://downloads.metabase.com/latest/metabase.jar -o metabase.jar
+
+echo "Iniciando Metabase..."
 java -jar metabase.jar
